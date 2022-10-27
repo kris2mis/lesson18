e5107466c2a74efcbc63c4542cf554335068b11f
NikolNat <102613704+NikolNat@users.noreply.github.com>
2022-10-27 21:48:10 +0300

    ad

--- account.py
+++ account.py
@@ -3,11 +3,17 @@
 
 
 class Account:
-    def init(self, id, balance, name, cofficient=0.01):
+    # constructor
+    def __init__(self, id, balance, name, cofficient=0.01):
         self.id = id
         self.balance = balance
         self.name = name
         self.cofficient = cofficient
+
+
+    # destructor (он 1 в отличие от конструктора)
+    # def __del__(self):
+    #     pass
 
     def get_info(self):
         return (f"{self.id}: "
